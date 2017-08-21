fe58955a687a145c4713b3075c33efbc8d48eb3d
William R Poulson <jessica.spildener@gmail.com>
2017-08-21 19:51:13 -0400

    gifs display 3 to row

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,10 @@
-var dogsArray = ["boston terrier", "corgi", "bulldog", "golden retriever"];
+var dogsArray = ["boston terrier", "corgi", "bulldog", "golden retriever", "pug"];
 
 function generateGifs() {
     var dogs = $(this).attr("nameData");
 
     //alert(dogs);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dogs + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dogs + "&api_key=dc6zaTOxFJmzC&limit=12";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -12,11 +12,15 @@ function generateGifs() {
         var results = response.data;
         $("#dogsView").empty();
         for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
+            var gifDiv = $("<div class='col-md-4'>");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
             var dogImage = $("<img>");
-            dogImage.attr("src", results[i].images.fixed_height.url);
+            dogImage.attr("src", results[i].images.fixed_height_still.url);
+            $(dogImage).attr("data-state", "still");
+            $(dogImage).attr("data-animate", results[i].images.fixed_height.url);
+            $(dogImage).attr("data-still", results[i].images.fixed_height_still.url);
+            $(dogImage).addClass("gif");
             gifDiv.append(p);
             gifDiv.append(dogImage);
             $("#dogsView").append(gifDiv);
@@ -43,6 +47,18 @@ $("#addDog").on("click", function(event) {
         $("form").trigger("reset");
     }
 });
+$("body").on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+
 $(document).on("click", ".dogBreed", generateGifs);
 
 createButtons();
